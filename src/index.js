2ab5ef03cb9c78940d028334c49b8bed7e463116
Aryyan7385 <viraj39singh@gmail.com>
2026-01-29 02:28:07 +0530

    Clarify CSS import with a comment
    Added a comment to clarify the CSS import statement.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+// Inside your src/index.js file
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './pages/index.css';
+import './pages/index.css'; // This looks into the pages folder for your styles
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
